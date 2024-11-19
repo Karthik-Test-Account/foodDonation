40035005382bfa22144d15306168559ca22d09ee
Karthik Ram <shegurikarthik@gmail.com>
2024-11-19 23:24:43 +0530

    home image added

--- controllers/donatorController.js
+++ controllers/donatorController.js
@@ -1,19 +1,24 @@
 const userModel = require('../models/userModel');
-const url = require("url")
 
 const submitDonationController = async (req, res) => {
     try {
         const { name, phone, email, foodType, quantity, pickupLocation, assurity } = req.body;
-        console.log(req.body);
+        console.log("from  donation controller:",req.body);
         // Check if user already exists
         const user = await userModel.findOne({ email });
         if (!user) {
-            return res.redirect('/all-donations?success=false&message=User%20not%20registered%20so%20cannot%20donate');
+            return res.render('all-donations',{
+                success:false,
+                message:'User not registered, so cannot donate',
+            });
         }
         
         // Check if name and phone match the user's info
         if (name !== user.name || phone !== user.phone) {
-            return res.redirect("/all-donations?success=false&message=Invalid%20Credentials");
+            return res.render("all-donations",{
+                success:false,
+                message:'Invalid Credentials',
+            });
         }
 
         // Create new donation object
@@ -26,20 +31,17 @@ const submitDonationController = async (req, res) => {
         await user.save();
         console.log("Donation saved successfully.");
 
-        // Redirect to the all-donations page with a success message
-        // res.redirect(`/all-donations?success=true&email=${email}`);
-
-        res.redirect(url.format({
-            pathname:"/all-donations",
-            query:{
-                success:true,
-                email: email,
-                user
-            }
-        }))
+        res.render('all-donations',{
+            success:true,
+            message:'Donation saved successfully',
+            user
+        })
     } catch (error) {
         console.error("Error saving donation:", error);
-        res.redirect('/all-donations?success=false&message=Something%20went%20wrong');
+        res.render('all-donations',{
+            success:false,
+            message:'Something went wrong'
+        });
     }
 };
 
